756136ef9717da57fb4321f190ac8463cf2b7ae3
Sofía Tello <tellosofia@outlook.com>
2025-11-02 14:27:33 +0100

    Exercise 24: Solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -4,17 +4,26 @@ import java.util.Stack;
 
 public class Exercise {
     public static Stack<String> createStack() {
+        Stack<String> newStack = new Stack<>();
 
+        newStack.push("Smith");
+        newStack.push("Montessori");
+        newStack.push("Peralta");
+        newStack.push("House");
+
+        return newStack;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
-
+        while (!stack.isEmpty()){ // stack changes dynamically - best to use while
+            System.out.println("Popped element: " + stack.pop());
+        }
+//        for (int i = 0; i < stack.size() + 3; i++) {
+//            System.out.println("Popped element: " + stack.pop());
+//        } --> not the best option
     }
 
     public static void main(String[] args) {
-
+        printAndEmptyStack(createStack());
     }
-
-
-
 }
